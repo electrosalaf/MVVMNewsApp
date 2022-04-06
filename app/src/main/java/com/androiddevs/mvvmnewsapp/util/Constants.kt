e5eb37cd2fd5ad71a9207bdc0b1e363c1e0640ca
electrosalaf <51977981+electrosalaf@users.noreply.github.com>
2022-04-06 14:20:34 +0100

    Starter Project

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+
+    companion object {
+        const val API_KEY = "8e51d0075daa4b8c89749c22a3ae7c56"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
