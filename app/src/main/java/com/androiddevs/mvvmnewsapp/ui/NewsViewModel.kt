631454c85c7e8ab4bfd74b1997b135a7a691c0eb
electrosalaf <51977981+electrosalaf@users.noreply.github.com>
2022-04-09 15:09:57 +0100

    project updated

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
@@ -9,19 +9,25 @@ import com.androiddevs.mvvmnewsapp.util.Resource
 import kotlinx.coroutines.launch
 import retrofit2.Response
 
-class NewsViewModel(val newsRepository: NewsRepository) : ViewModel() {
+class NewsViewModel(
+    private val newsRepository: NewsRepository
+) : ViewModel() {
 
     val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
     var breakingNewsPage = 1
 
-    fun getBreakingNews(countryCode: String) = viewModelScope.launch {
-        breakingNews.postValue                                 (Resource.Loading())
+    init {
+        getBreakingNews("ng")
+    }
+
+    private fun getBreakingNews(countryCode: String) = viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
         val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
-        breakingNews.postValue(handleBreakingNewsResponse(response))
+        breakingNews.postValue(handleBreakingNewsResponse(response as Response<NewsResponse>))
     }
 
     private fun handleBreakingNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse> {
-        if (response.isSuccessful) {
+        if(response.isSuccessful) {
             response.body()?.let { resultResponse ->
                 return Resource.Success(resultResponse)
             }
